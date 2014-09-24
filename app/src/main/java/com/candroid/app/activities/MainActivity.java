30c3677eece0cbeaecf75e3ab0b250e3b71eca34
John Shelley <john.shelley.p@gmail.com>
2014-09-23 22:14:20 -0600

    (action-bar) add start in action bar to clean up
    Also create a Vermeer Launch Icon ;)

--- app/src/main/java/com/candroid/app/activities/MainActivity.java
+++ app/src/main/java/com/candroid/app/activities/MainActivity.java
@@ -2,6 +2,9 @@ package com.candroid.app.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -9,7 +12,6 @@ import android.widget.ListView;
 import com.candroid.app.R;
 import com.candroid.app.dto.Macro;
 import com.candroid.app.dto.ObjectPool;
-import com.candroid.app.util.XMLPullParserHandler;
 import com.cengalabs.flatui.FlatUI;
 import com.cengalabs.flatui.views.FlatButton;
 import com.crashlytics.android.Crashlytics;
@@ -17,48 +19,23 @@ import com.crashlytics.android.Crashlytics;
 import org.simpleframework.xml.Serializer;
 import org.simpleframework.xml.core.Persister;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 public class MainActivity extends Activity {
 
-    FlatButton button;
-
     ListView listView;
-
-    List<Macro> macroList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Crashlytics.start(this);
         setContentView(R.layout.activity_main);
-        button = (FlatButton) findViewById(R.id.button_pull_data);
         listView = (ListView) findViewById(R.id.list);
         FlatUI.setDefaultTheme(FlatUI.DARK);
         FlatUI.setActionBarTheme(this, FlatUI.DARK, false, false);
-        init();
     }
 
-    private void init(){
-        setListeners();
-    }
-
-    private void setListeners(){
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                button_pull_data();
-            }
-        });
-    }
-
-    void button_pull_data() {
-        parseObjectPoolPULL();
-    }
-
-    private void parseObjectPoolPULL() {
+    private void mockInitialData() {
         // Local file example
         try {
             Serializer serializer = new Persister();
@@ -66,7 +43,6 @@ public class MainActivity extends Activity {
             ObjectPool objectPool = serializer.read(ObjectPool.class, getAssets().open("display.xml"));
             //XMLPullParserHandler parserHandler = new XMLPullParserHandler();
             //macroList = parserHandler.parse(getAssets().open("display.xml"));
-
             ArrayAdapter<Macro> adapter = new ArrayAdapter<Macro>(this, R.layout.list_item, objectPool.macros);
             listView.setAdapter(adapter);
         } catch (IOException e) {
@@ -76,4 +52,22 @@ public class MainActivity extends Activity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_start:
+                mockInitialData();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
